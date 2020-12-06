8195d859ec574454c513b00922950a71f117a871
Airways13 <73844203+Airways13@users.noreply.github.com>
2020-12-06 11:34:07 +0500

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,15 @@
+import telebot
+bot = telebot. telebot('1431807223:AAE6vm8eEUV6qQzRdz-142fkO6p4tCMN4qQ')
+def send():
+	text =massage.text
+	if text =='sublime text3':
+		bot.send_massage(massage.from_user.id,
+	elif text == 'java':
+		bot.send_massage(massage.from_user.id, 
+	elif text == 'php':	
+		bot.send_massage(massage.from_user.id, 
+	elif text == 'c#' :
+		bot.send_massage(massage.from_user.id, 
+	else: bot.send_massage(massage.from_user.id ,bot.polling(none_stop= true)
+
+
